375828c79a2f20685719966401dd530e9b12012a
闻家奇 <939949243@qq.com>
2019-11-13 15:22:18 +0800

    job定时任务

---
+++ src/main/java/com/gsm/job/MainJob.java
@@ -0,0 +1,25 @@
+package com.gsm.job;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import java.util.Date;
+import java.util.SimpleTimeZone;
+
+//主要用于标记配置类，兼备Component的效果
+@Configuration
+/**
+ * 开启定时任务
+ */
+@EnableScheduling
+public class MainJob {
+
+    /**
+     * 添加定时任务，
+     */
+    @Scheduled(cron = "0 0/1 * * * ?")
+    public void myJob(){
+        System.out.println(new Date());
+    }
+}
